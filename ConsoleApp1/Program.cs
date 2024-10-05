3fbb52cd13187578202de88509b9cb3e6607ac85
valer <valer@DESKTOP-QD94NVR>
2024-10-05 20:00:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        // Ввод положительного вещественного числа
+        double x = 27.3198;
+
+        // Извлечение дробной части x
+        double fractionalPart = x - Math.Truncate(x);
+
+        // Преобразование дробной части в строку
+        string fractionalPartStr = fractionalPart.ToString();
+
+        // Удаление ведущего "0." из строки
+        fractionalPartStr = fractionalPartStr.Substring(2);
+
+        // Получение первого символа строки (т.е. первой цифры дробной части)
+        char firstDigit = fractionalPartStr[0];
+
+        // Преобразование символа в целое число
+        int d = int.Parse(firstDigit.ToString());
+
+        // Вывод результата
+        Console.WriteLine("Первая цифра дробной части: " + d);
+    }
+}
